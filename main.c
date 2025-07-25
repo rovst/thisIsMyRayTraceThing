aac1072bae49f1ead7b21704a03d0b59bc8135b8
rovst <loganliunz@gmail.com>
2025-07-26 08:50:06 +1200

    Update main.c

--- main.c
+++ main.c
@@ -1,84 +1,115 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <SDL2/SDL.h>
 #include <math.h>
+#include <stdbool.h>  // For bool
 
-#define COLOR_WHITE 0xFFFFFF
-
-// Structure to represent a circle
 struct Circle {
-    double x;
-    double y;
-    double r;
+    int x;
+    int y;
+    int r;
 };
 
-// Function to fill a circle on an SDL surface
+
+struct ray{
+    double startY;
+    double angle;
+    double startX;
+    double endX;
+    double endY;
+};
+
+
 void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 color) {
-    double radius_squared = pow(circle.r, 2);
+    int x_start = circle.x - circle.r;
+    int x_end = circle.x + circle.r;
+    int y_start = circle.y - circle.r;
+    int y_end = circle.y + circle.r;
 
-    // Iterate over a square bounding box around the circle
-    for (int x = (int)(circle.x - circle.r); x <= (int)(circle.x + circle.r); x++) {
-        for (int y = (int)(circle.y - circle.r); y <= (int)(circle.y + circle.r); y++) {
-            // Calculate the squared distance from the center of the circle to the current pixel
-            double distance_squared = pow(x - circle.x, 2) + pow(y - circle.y, 2);
+    int radius_squared = circle.r * circle.r;
 
-            // If the pixel is inside or on the boundary of the circle, fill it
+    for (int x = x_start; x <= x_end; x++) {
+        for (int y = y_start; y <= y_end; y++) {
+            int dx = x - circle.x;
+            int dy = y - circle.y;
+            int distance_squared = dx * dx + dy * dy;
+
             if (distance_squared <= radius_squared) {
-                // Ensure the pixel is within the surface boundaries
-                if (x >= 0 && x < surface->w && y >= 0 && y < surface->h) {
-                    // Get the pixel format to map the color correctly
-                    Uint32* pixels = (Uint32*)surface->pixels;
-                    pixels[y * surface->w + x] = color;
-                }
+                SDL_Rect pixel = { x, y, 1, 1 };
+                SDL_FillRect(surface, &pixel, color);
             }
         }
     }
 }
 
+void emmit_Ray(struct Circle circle, struct ray rayarr[100]){
+
+    for(int i=1; i<=100;i++){
+        double angle=(i/100.0)*360.0;
+        rayarr[i].angle=angle;
+        printf("%f\n", angle);
+    }
+}
+
 int main() {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create an SDL window
-    SDL_Window* window = SDL_CreateWindow("Raytrace Circle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 700, 0);
-    if (window == NULL) {
-        fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    SDL_Window *window = SDL_CreateWindow(
+        "this is my raytrace thing", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        900, 700, 
+        0);
+
+    if (!window) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    // Get the surface of the window
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    if (surface == NULL) {
-        fprintf(stderr, "Could not get window surface! SDL_Error: %s\n", SDL_GetError());
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    if (!surface) {
+        printf("SDL_GetWindowSurface Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
 
-    // Fill the entire surface with black (or any background color)
-    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0)); // Black background
+    bool running = true;
+    SDL_Event event;
 
-    // Define a circle
-    struct Circle myCircle = {
-        .x = 450, // Center X
-        .y = 350, // Center Y
-        .r = 100  // Radius
-    };
+    // Define the circle
+    struct Circle circle = { 200, 200, 80 };
+    struct Circle shadow_circle = { 200, 20, 200 };
 
-    // Call FillCircle with the correct arguments
-    // Use SDL_MapRGB to get the correct pixel format for the color
-    FillCircle(surface, myCircle, SDL_MapRGB(surface->format, 255, 255, 255)); // White circle
+    struct ray rays[100];
+    emmit_Ray(circle, rays);
 
-    // Update the window surface to show the changes
-    SDL_UpdateWindowSurface(window);
+    Uint32 white = SDL_MapRGB(surface->format, 255, 255, 255);
+    Uint32 black = SDL_MapRGB(surface->format, 0, 0, 0);
 
-    // Keep the window open for 5 seconds
-    SDL_Delay(5000);
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            if(event.type ==SDL_MOUSEMOTION && event.motion.state !=0){
+                circle.x=event.motion.x;
+                circle.y=event.motion.y;
+            }
+    }
+        //background
+        SDL_FillRect(surface, NULL, black);
+        // circle
+        FillCircle(surface, circle, white);
+        FillCircle(surface, shadow_circle, white);
 
-    // Clean up SDL resources
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(16); // frames
+    }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
 
